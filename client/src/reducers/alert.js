f2ec89b069a433325dbee4085767858961bd5da5
Stefan Kovarbasic <kovar.zmaj@gmail.com>
2020-06-29 00:54:05 +0200

    MERN stack initial commit

---
+++ client/src/reducers/alert.js
@@ -0,0 +1,17 @@
+import * as actionTypes from '../actions/types';
+
+const initialState = [];
+
+export default function (state = initialState, action) {
+  const { type, payload } = action;
+  const { SET_ALERT, REMOVE_ALERT } = actionTypes;
+
+  switch (type) {
+    case SET_ALERT:
+      return [...state, payload];
+    case REMOVE_ALERT:
+      return state.filter(alert => alert.id !== payload);
+    default:
+      return state;
+  }
+}
